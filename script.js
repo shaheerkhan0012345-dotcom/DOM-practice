358ed620cca7c99e3612d6c374728290053e38a0
Muammad Shaheer <shaheerkhan0012345@gmail.com>
2026-07-03 08:57:34 -0700

    dome practice

---
+++ script.js
@@ -0,0 +1,16 @@
+let heading = document.getElementById("heading")
+
+heading.textContent = "Hello Kake"
+
+
+
+let text = document.getElementsByClassName("text")
+
+text[0].textContent = "Always Op"
+text[1].textContent = "Chacha"
+
+let button = document.getElementsByTagName("ul")
+
+button[0].textContent = "goku"
+button[1].textContent = "gohan"
+button[2].textContent = "gohan"
